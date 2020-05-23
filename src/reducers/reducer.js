90b6b9de82036708c4f460c56a732adcdd2b00a8
Gordon Preumont <preumontgr@gmail.com>
2020-05-24 01:16:46 +0200

    reducers

---
+++ src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const houses = [
+    'Serpentard',
+    'Griffondor',
+    'Serdaigle',
+    'Pouffsoufle',
+]
+
+const reducer = (state, action) => ({
+    houses,
+    selectedHouses:  houses[Math.floor(Math.random() * 4)]
+})
+
+export default reducer;
